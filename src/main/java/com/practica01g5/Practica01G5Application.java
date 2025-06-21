78bb3a6cdb3188b18f33d73980e3f824a2d967f1
Tomas-2111 <tojoalgu@gmail.com>
2025-06-21 16:32:11 -0600

    Initial commit

---
+++ src/main/java/com/practica01g5/Practica01G5Application.java
@@ -0,0 +1,13 @@
+package com.practica01g5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Practica01G5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Practica01G5Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/practica01g5/Practica01G5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practica01g5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Practica01G5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
